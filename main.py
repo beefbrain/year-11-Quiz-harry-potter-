f18ba224565c201f81d705ba78f170d68708f283
Sneha Safayet <100541586+beefbrain@users.noreply.github.com>
2022-03-08 04:48:41 +0000

    I was confuse on how i could greet the player but continue with context. so i just made multiple print statements

---
+++ main.py
@@ -0,0 +1,4 @@
+name = input("what's your name ")
+print("Hello {}".format(name)) 
+print ("welcome to this harry potter quiz")
+print ("In this quiz you will find out who is your hogwarts best friend and the wand core you will most likely have")
