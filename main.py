24b3a1a11b203523554afe42712f1b9c8207086c
Sneha Safayet <100541586+beefbrain@users.noreply.github.com>
2022-03-14 04:03:42 +0000

    I made the introduction more aesthetically pleasing thus younger players would find it engaging

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input("what's your name ")
+print("ʕ•́ᴥ•̀ʔっ♡ Hello {}".format(name)) 
+print ("welcome to this harry potter quiz ʕ•́ᴥ•̀ʔっ♡ ")
+print ("Harry potter is a book series written by auther JK Rowlings.She wrote a total of 7 books but this quiz is based on 6 of them")
+print("   ")
+print ("          .。･:*:･(✿ ◕ 3 ◕ ) ❤ ( ◕ ε ◕ ✿ )･:*:･。.")
+print("   ")
+print ("You'll be instructed to press a number that equals your option, at the end of the quiz, you will find out who your hogwarts    best friend is!")
